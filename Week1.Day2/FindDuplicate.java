b5b3dc03bc95723e5627144555eb87c587a2555d
ragav6599 <120043838+ragav6599@users.noreply.github.com>
2023-06-28 18:14:44 +0530

    Update FindDuplicate.java

--- Week1.Day2/FindDuplicate.java
+++ Week1.Day2/FindDuplicate.java
@@ -2,7 +2,7 @@ package week1.day2;
 
 public class FindDuplicate {
 	public static void main(String[] args) {
-		int[] nums = {2, 5, 7, 7, 5, 9, 2, 3};
+		int[] nums = {14,12,13,11,15,14,18,16,17,19,18,17,20};
 		for (int i = 0; i < nums.length; i++) {
 			
 			for (int j = i+1; j < nums.length; j++) {
